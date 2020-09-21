5f98c3bc80a2841873f2899ea9038e2245efc41f
RitionaMuscat <ritiona.muscat.a100244@mcast.edu.mt>
2020-09-21 14:57:26 +0200

    added the HttpRequestMethods

--- HomeAssignment/app/src/main/java/com/example/homeassignment/MainActivity.java
+++ HomeAssignment/app/src/main/java/com/example/homeassignment/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.homeassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new GetProducts().execute();
+    }
+
+    public class GetProducts extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
     }
 }
+
