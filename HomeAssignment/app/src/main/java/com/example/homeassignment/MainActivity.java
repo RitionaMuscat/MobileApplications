882962464953eeefa72371716211ecdb8f1903bd
RitionaMuscat <ritiona.muscat.a100244@mcast.edu.mt>
2020-09-24 07:56:05 +0200

    getting data

--- HomeAssignment/app/src/main/java/com/example/homeassignment/MainActivity.java
+++ HomeAssignment/app/src/main/java/com/example/homeassignment/MainActivity.java
@@ -32,12 +32,25 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
     ArrayList<HashMap<String, String>> MovieList = new ArrayList<HashMap<String, String>>();
+
+
+    public static ArrayList<String> titleArr = new ArrayList<String>();
+    public static ArrayList<String> popularityArr = new ArrayList<String>();
+    public static ArrayList<String> voteCountArr = new ArrayList<String>();
+    public static ArrayList<String> videoArr = new ArrayList<String>();
+    public static ArrayList<String> posterArr = new ArrayList<String>();
+    public static ArrayList<String> idArr = new ArrayList<String>();
+    public static ArrayList<String> adultArr = new ArrayList<String>();
+    public static ArrayList<String> backdropArr = new ArrayList<String>();
+    public static ArrayList<String> languageArr = new ArrayList<String>();
+    public static ArrayList<String> origTitleArr = new ArrayList<String>();
+    public static ArrayList<String> voteAvgArr = new ArrayList<String>();
+    public static ArrayList<String> overViewArr = new ArrayList<String>();
+    public static ArrayList<String> releaseDateArr = new ArrayList<String>();
+
     HashMap<String, String> Movies = new HashMap<>();
-    public static  ArrayList<String> titleArr = new ArrayList<String>();
-    public static   HashMap<String, String> MovieTitle = new HashMap<>();
-
-
 
 
     @Override
@@ -81,29 +94,28 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
-    public class GetMovies extends AsyncTask<Void,Void,Void> {
+    public class GetMovies extends AsyncTask<Void, Void, Void> {
 
-        protected  void onPreExecute(){
+        protected void onPreExecute() {
             super.onPreExecute();
             Toast.makeText(MainActivity.this, "Json Data is downloading", Toast.LENGTH_LONG).show();
         }
+
         @Override
         protected Void doInBackground(Void... voids) {
             String ApiKey = "269f0b365935d94450397fa37668f42b";
-            String url = "https://api.themoviedb.org/3/movie/now_playing?api_key="+ApiKey;
+            String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + ApiKey;
 
             HttpClient sh = new HttpClient();
             String jsonStr = sh.makeServiceCall(url);
 
             Log.e("TAG", "Response from url: " + jsonStr);
-            if(jsonStr != null)
-            {
-                try{
+            if (jsonStr != null) {
+                try {
                     JSONObject jsonObject = new JSONObject(jsonStr);
                     JSONArray arrJson = jsonObject.getJSONArray("results");
 
-                    for (int i = 0; i<arrJson.length();i++)
-                    {
+                    for (int i = 0; i < arrJson.length(); i++) {
                         JSONObject c = arrJson.getJSONObject(i);
                         String popularity = c.getString("popularity");
                         String vote_count = c.getString("vote_count");
@@ -120,38 +132,48 @@ public class MainActivity extends AppCompatActivity {
                         String release_date = c.getString("release_date");
 
 
-                        Movies.put("popularity",popularity);
-                        Movies.put("vote_count",vote_count);
-                        Movies.put("video",video);
-                        Movies.put("poster_path",poster_path);
-                        Movies.put("id",id);
-                        Movies.put("adult",adult);
-                        Movies.put("backdrop_path",backdrop_path);
-                        Movies.put("original_language",original_language);
-                        Movies.put("original_title",original_title);
-                        Movies.put("title",title);
-                        Movies.put("vote_average",vote_average);
-                        Movies.put("overview",overview);
-                        Movies.put("release_date",release_date);
+                        Movies.put("popularity", popularity);
+                        Movies.put("vote_count", vote_count);
+                        Movies.put("video", video);
+                        Movies.put("poster_path", poster_path);
+                        Movies.put("id", id);
+                        Movies.put("adult", adult);
+                        Movies.put("backdrop_path", backdrop_path);
+                        Movies.put("original_language", original_language);
+                        Movies.put("original_title", original_title);
+                        Movies.put("title", title);
+                        Movies.put("vote_average", vote_average);
+                        Movies.put("overview", overview);
+                        Movies.put("release_date", release_date);
 
                         MovieList.add(Movies);
 
-                        MovieTitle.put("title",title);
-                        titleArr.add(MovieTitle.get("title"));
-                        Log.e("TAG", "Response from MovieTitle in main: " + MovieTitle.get("title"));
-                     Log.e("TAG", "Response from MovieTitle in main: " + titleArr);
+
+                        titleArr.add(Movies.get("title"));
+                        popularityArr.add(Movies.get("popularity"));
+                        voteCountArr.add(Movies.get("vote_count"));
+                        posterArr.add(Movies.get("poster_path"));
+                        idArr.add(Movies.get("id"));
+                        adultArr.add(Movies.get("adult"));
+                        backdropArr.add(Movies.get("backdrop_path"));
+                        languageArr.add(Movies.get("original_language"));
+                        origTitleArr.add(Movies.get("original_title"));
+                        voteAvgArr.add(Movies.get("vote_average"));
+                        overViewArr.add(Movies.get("overview"));
+                        releaseDateArr.add(Movies.get("release_date"));
+                        videoArr.add(Movies.get("video"));
 
 
 
                     }
-                } catch ( JSONException e) {
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
             return null;
         }
-        protected void onPostExecute (Void result)
-        {
+
+        protected void onPostExecute(Void result) {
             NavigationView navigationView = findViewById(R.id.nav_view);
             navigationView.setNavigationItemSelectedListener(
                     new NavigationView.OnNavigationItemSelectedListener() {
@@ -159,9 +181,21 @@ public class MainActivity extends AppCompatActivity {
                         public boolean onNavigationItemSelected(MenuItem menuItem) {
                             int id = menuItem.getItemId();
                             if (id == R.id.nav_popularMovies) {
-                                 Intent MOVIE = new Intent(getApplicationContext(), MoviesActivity.class);
-                                 MOVIE.putStringArrayListExtra("titleArr", titleArr);
-                                 startActivity(MOVIE);
+                                Intent MOVIE = new Intent(getApplicationContext(), MoviesActivity.class);
+                                MOVIE.putStringArrayListExtra("titleArr", titleArr);
+                                MOVIE.putStringArrayListExtra("popularityArr", popularityArr);
+                                MOVIE.putStringArrayListExtra("voteCountArr", voteCountArr);
+                                MOVIE.putStringArrayListExtra("posterArr", posterArr);
+                                MOVIE.putStringArrayListExtra("idArr", idArr);
+                                MOVIE.putStringArrayListExtra("adultArr", adultArr);
+                                MOVIE.putStringArrayListExtra("backdropArr", backdropArr);
+                                MOVIE.putStringArrayListExtra("languageArr", languageArr);
+                                MOVIE.putStringArrayListExtra("origTitleArr", origTitleArr);
+                                MOVIE.putStringArrayListExtra("voteAvgArr", voteAvgArr);
+                                MOVIE.putStringArrayListExtra("overViewArr", overViewArr);
+                                MOVIE.putStringArrayListExtra("releaseDateArr", releaseDateArr);
+                                MOVIE.putStringArrayListExtra("videoArr", videoArr);
+                                startActivity(MOVIE);
 
                             }
                             DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- HomeAssignment/app/src/main/java/com/example/homeassignment/MoviesActivity.java
+++ HomeAssignment/app/src/main/java/com/example/homeassignment/MoviesActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,9 +22,22 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MoviesActivity extends AppCompatActivity {
-    String MovieList = new String();
-    ArrayList<String> popularity = new ArrayList<>();
-    ArrayList<String> test = new ArrayList<>();
+    String selectedMovie = "";
+    Integer movieIndex = null;
+
+    ArrayList<String> MoviesNames = new ArrayList<String>();
+    ArrayList<String> popularityArr = new ArrayList<String>();
+    ArrayList<String> voteCountArr = new ArrayList<String>();
+    ArrayList<String> videoArr = new ArrayList<String>();
+    ArrayList<String> posterArr = new ArrayList<String>();
+    ArrayList<String> idArr = new ArrayList<String>();
+    ArrayList<String> adultArr = new ArrayList<String>();
+    ArrayList<String> backdropArr = new ArrayList<String>();
+    ArrayList<String> languageArr = new ArrayList<String>();
+    ArrayList<String> origTitleArr = new ArrayList<String>();
+    ArrayList<String> voteAvgArr = new ArrayList<String>();
+    ArrayList<String> overViewArr = new ArrayList<String>();
+    ArrayList<String> releaseDateArr = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +49,79 @@ public class MoviesActivity extends AppCompatActivity {
         final Spinner spnMovC = findViewById(R.id.spinner);
 
         Intent i = getIntent();
-        ArrayList<String> items_list = new ArrayList<String>();
-        items_list = i.getStringArrayListExtra("titleArr");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, items_list);
+        MoviesNames   = i.getStringArrayListExtra("titleArr");
+        popularityArr   = i.getStringArrayListExtra("popularityArr");
+        voteCountArr  = i.getStringArrayListExtra("voteCountArr");
+        videoArr      = i.getStringArrayListExtra("videoArr");
+        posterArr     = i.getStringArrayListExtra("posterArr");
+        idArr         = i.getStringArrayListExtra("idArr");
+        adultArr      = i.getStringArrayListExtra("adultArr");
+        backdropArr   = i.getStringArrayListExtra("backdropArr");
+        languageArr   = i.getStringArrayListExtra("languageArr");
+        origTitleArr  = i.getStringArrayListExtra("origTitleArr");
+        voteAvgArr    = i.getStringArrayListExtra("voteAvgArr");
+        overViewArr   = i.getStringArrayListExtra("overViewArr");
+        releaseDateArr   = i.getStringArrayListExtra("releaseDateArr");
+
+
+        Log.e("TAG", "Response from popularityArr: " + popularityArr);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, MoviesNames);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spnMovC.setAdapter(adapter);
+
+        spnMovC.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedMovie = spnMovC.getSelectedItem().toString();
+                movieIndex = MoviesNames.indexOf(selectedMovie);
+                if(MoviesNames.contains(selectedMovie))
+                {
+                    TextView Popularity;
+                    Popularity = findViewById(R.id.popularity);
+                    Popularity.setText(popularityArr.get(movieIndex));
+
+                    TextView vote;
+                    vote = findViewById(R.id.vote);
+                    vote.setText(voteAvgArr.get(movieIndex));
+
+                    TextView voteC;
+                    voteC = findViewById(R.id.voteC);
+                    voteC.setText(voteCountArr.get(movieIndex));
+
+                    TextView overView;
+                    overView = findViewById(R.id.OverView);
+                    overView.setText(overViewArr.get(movieIndex));
+
+                    TextView backdrop;
+                    backdrop = findViewById(R.id.backdrop);
+                    backdrop.setText(backdropArr.get(movieIndex));
+
+                    TextView language;
+                    language = findViewById(R.id.language);
+                    language.setText(languageArr.get(movieIndex));
+
+                    TextView origTitle;
+                    origTitle = findViewById(R.id.origTitle);
+                    origTitle.setText(origTitleArr.get(movieIndex));
+
+                    TextView video;
+                    video = findViewById(R.id.video);
+                    video.setText(videoArr.get(movieIndex));
+
+                    TextView poster;
+                    poster = findViewById(R.id.poster);
+                    poster.setText(posterArr.get(movieIndex));
+                }
+}
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
+
 }
 
